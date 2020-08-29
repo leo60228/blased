c63d5d5cf8f49b3356db0f22cdfcf584d6e52c71
leo60228 <leo@60228.dev>
2020-08-28 20:40:07 -0400

    more stuff

--- examples/login.rs
+++ examples/login.rs
@@ -6,7 +6,11 @@ async fn main() -> Result<()> {
     let mut args = args().skip(1);
     let user = args.next().unwrap();
     let pass = args.next().unwrap();
-    let _client = AuthenticatedClient::user_pass(&user, &pass).await?;
+    let client = AuthenticatedClient::user_pass(&user, &pass).await?;
+    let user = client.get_user().await?;
+    println!("{:#?}", user);
+    let team = client.get_team(&user.favorite_team).await?;
+    println!("{:#?}", team);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(backtrace)]
 
+use async_trait::async_trait;
+use chrono::prelude::*;
 use http_types::cookies::Cookie;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::backtrace::Backtrace;
 use std::result::Result as StdResult;
 use surf::Client;
@@ -24,6 +26,12 @@ pub enum Error {
     Cookie {
         #[from]
         source: cookie::ParseError,
+        backtrace: Backtrace,
+    },
+    #[error("url encode error: {source}")]
+    UrlEncode {
+        #[from]
+        source: serde_urlencoded::ser::Error,
         backtrace: Backtrace,
     },
     #[error("bad response")]
@@ -62,7 +70,7 @@ impl Default for UnauthenticatedClient {
 #[derive(Debug)]
 pub struct AuthenticatedClient {
     client: Client,
-    token: String,
+    token: Cookie<'static>,
 }
 
 impl AuthenticatedClient {
@@ -100,14 +108,50 @@ impl AuthenticatedClient {
             .ok_or_else(|| Error::BadResponse {
                 backtrace: Backtrace::capture(),
             })?;
-        let token = cookie.value().to_string();
+        let token = cookie.into_owned();
 
         Ok(Self { client, token })
     }
 }
 
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct Team {
+    pub id: String,
+    pub lineup: [String; 9],
+    pub rotation: [String; 5],
+    pub bullpen: [String; 8],
+    pub bench: [String; 3],
+    pub season_attributes: Vec<String>,
+    pub permanent_attributes: Vec<String>,
+    pub full_name: String,
+    pub location: String,
+    pub main_color: String,
+    pub nickname: String,
+    pub secondary_color: String,
+    pub shorthand: String,
+    pub emoji: String,
+    pub slogan: String,
+    pub shame_runs: usize,
+    pub total_shames: usize,
+    pub total_shamings: usize,
+    pub season_shames: usize,
+    pub season_shamings: usize,
+    pub championships: usize,
+}
+
+#[async_trait]
 pub trait Blaseball {
     fn get_client(&self) -> &Client;
+
+    async fn get_team(&self, team: &str) -> Result<Team> {
+        let mut req = self
+            .get_client()
+            .get("https://blaseball.com/database/team")
+            .build();
+        req.set_query(&[("id", team)])?;
+        Ok(self.get_client().send(req).await?.body_json().await?)
+    }
 }
 
 impl Blaseball for UnauthenticatedClient {
@@ -122,9 +166,49 @@ impl Blaseball for AuthenticatedClient {
     }
 }
 
-pub trait AuthenticatedBlaseball: Blaseball {}
+#[derive(Deserialize, Debug, PartialEq, Eq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: String,
+    pub email: String,
+    pub apple_id: Option<String>,
+    pub google_id: Option<String>,
+    pub facebook_id: Option<String>,
+    pub name: Option<String>,
+    pub coins: usize,
+    pub votes: usize,
+    pub created: DateTime<Utc>,
+    pub favorite_team: String,
+    pub unlocked_shop: bool,
+    pub unlocked_election: bool,
+    pub daily_coins_tier: usize,
+    pub begs: usize,
+    pub max_bet_tier: usize,
+    pub peanuts: usize,
+    pub peanuts_eaten: usize,
+    pub squirrels: usize,
+}
 
-impl AuthenticatedBlaseball for AuthenticatedClient {}
+#[async_trait]
+pub trait AuthenticatedBlaseball: Blaseball {
+    fn get_token(&self) -> &Cookie;
+
+    async fn get_user(&self) -> Result<User> {
+        Ok(self
+            .get_client()
+            .get("https://www.blaseball.com/api/getUser")
+            .header("Cookie", self.get_token().to_string())
+            .await?
+            .body_json()
+            .await?)
+    }
+}
+
+impl AuthenticatedBlaseball for AuthenticatedClient {
+    fn get_token(&self) -> &Cookie {
+        &self.token
+    }
+}
 
 #[cfg(test)]
 mod tests {
